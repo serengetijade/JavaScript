438f289fc6a578328b8f90dd92e17cd19588975e
Jade Abreu <110346143+serengetijade@users.noreply.github.com>
2022-08-29 08:40:12 -0800

    Basic JavaScript initial

---
+++ Basic-JavaScript-Projects/Project1-expressions-alert/js/main.js
@@ -0,0 +1,20 @@
+alert("Hello again, World");
+
+var A=("This is a string- data that represents text."); //define variables with =
+
+var B=("Variables are a useful way to reference data with JavaScript.");
+
+//CONCATENATION
+var BB= "Concatenated" + " strings display on the same line:"; //use + to concatenate strings
+var Sent1=("This is the beginning of the string ");  //and or use + in the function
+var Sent2=("and this is the end of the string.");
+
+//DEFINE MULTIPLE VARIABLES
+var cat="Nemo", dog="Tuesday", fish="Krinklebine";  //define multiple variables
+
+//BASIC FUNCTIONS
+function My_First_Function() { //Difine a function and naming it
+    var str = "This is the button text!"; //Difine a variable and giving it a string value
+    document.getElementById("Button_Text").innerHTML = str; //Put the value of the variable into the HTML elementFromPoint with the "Button_Text" id
+}
+
