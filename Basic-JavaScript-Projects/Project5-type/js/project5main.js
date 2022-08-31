3d29cbe8c131d8eae951789fe24c530b8af44efd
Jade Abreu <110346143+serengetijade@users.noreply.github.com>
2022-08-30 19:30:05 -0800

    moved js to .js file

---
+++ Basic-JavaScript-Projects/Project5-type/js/project5main.js
@@ -0,0 +1,20 @@
+//TYPE COERSION
+document.getElementById("coersionresult").innerHTML("10"+5);
+
+//UNIQUE MATH VALUES
+function my_Function() {
+    document.getElementById("Test").innerHTML= 0/0;
+}
+//isNAN TEST
+
+//INFINITY
+
+//BOOLEAN LOGIC
+
+//NOT OPERATOR
+function not_function() {
+    document.getElementById("not").innerHTML = !(20>10)
+}
+
+
+//END MATH WITH JS
