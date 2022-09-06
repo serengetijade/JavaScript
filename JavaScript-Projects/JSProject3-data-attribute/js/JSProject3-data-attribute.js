6aa453ef42772961a067cef33a3f5bc21a0022f8
Jade Abreu <110346143+serengetijade@users.noreply.github.com>
2022-09-06 10:49:41 -0800

    JSProject3 notes

--- JavaScript-Projects/JSProject3-data-attribute/js/JSProject3-data-attribute.js
+++ JavaScript-Projects/JSProject3-data-attribute/js/JSProject3-data-attribute.js
@@ -1,10 +1,10 @@
 //DATA-* ATTRIBUTE
-function displayType(character) {
-    var characterType = character.getAttribute("data-character-type");
-    alert(characterType + " is in the " + character.innerHTML + " universe.");
+function displayType(character) {       //defines (character) as the parameter of the function
+    var characterType = character.getAttribute("data-character-type");  //initializes a variable and assigns it's value using the object method .getAttribute()
+    alert(characterType + " is in the " + character.innerHTML + " universe.");  //creates a string using variables defined above
 }
 
-function olMac(animal) {
+function olMac(animal) {        //defines (animal) as the parameter of the function
     var animalNoise = animal.getAttribute("data-animal-noise");
     alert("The " + animal.innerHTML + " says " + animalNoise)
 }
