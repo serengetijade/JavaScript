d84db38a96807583c5c26fefc4b670f49f18058f
SerengetiJade <jad245@gmail.com>
2023-02-03 10:04:56 -0800

    Center delete button vertically

--- ToDo-App/js/ToDo-App.js
+++ ToDo-App/js/ToDo-App.js
@@ -3,7 +3,7 @@ function get_todos() {
     var todos = new Array;  //instatiate an empty array
     var todos_str = localStorage.getItem('todo'); //Pull the tast that was saved in the web browser memory
     if (todos_str !== null) {   //If the input is not null, then JSON will parse comminicate with the web browser to make the task a JS object.
-        todos = JSON.parse(todos_str);
+        todos = JSON.parse(todos_str);  //JSON.parse turns each object in the array into a string
     }
     return todos;
 }
@@ -12,7 +12,7 @@ function get_todos() {
 function add() {
     var task= document.getElementById('task').value; //Create a variable using the value of a 'task'
     var todos = get_todos(); 
-    todos.push(task);   //Add a new task to the end of the array
+    todos.push(task);   //Use built-in push function to add a new task to the end of the array
     localStorage.setItem('todo', JSON.stringify(todos));    //Convert the task input to a JSON string
     document.getElementById("task").value = "";
     show();             //Create an undefined function
@@ -31,17 +31,17 @@ function show() {
 
     var buttons = document.getElementsByClassName('remove');
     for (var i=0; i < buttons.length; i++) {   
-        buttons[i].addEventListener('click', remove); //Remove the completed task when the "checkbox" is clicked.
+        buttons[i].addEventListener('click', remove); //Call the remove function to remove the completed task when the "checkbox" is clicked.
     }
 }
 
 document.getElementById('add').addEventListener('click', add); //Display the inputed task when the "Add Item" button is clicked.
 
-show(); //Keep the inputs displayed permanently on the screen.
+show(); //Call the show function to keep the inputs displayed permanently on the screen.
 
 //REMOVE TASK WHEN COMPLETED (CHECKED OFF)
 function remove() {
-    var id = this.getAttribute('id');
+    var id = this.getAttribute('id');   //Built-in HTML DOM method getAttribute returns value of given object attribute
     var todos = get_todos();
     todos.splice(id, 1);
     localStorage.setItem('todo', JSON.stringify(todos));
